bcdc30abd1532d8e8fcfcac469f0bf865ddaccf0
Yoav <yoav.benzvi@gmail.com>
2020-05-12 16:28:26 +0200

    Updates

---
+++ precache-manifest.3f4291988335c644d567b1c1451cf012.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b4939512559017d8c19b4044f66a2c6",
+    "url": "/Yoavbenzvi/todo-app/index.html"
+  },
+  {
+    "revision": "37e04be88b26e6e53ddf",
+    "url": "/Yoavbenzvi/todo-app/static/css/main.60db9259.chunk.css"
+  },
+  {
+    "revision": "e471cd66f89e1d28ba69",
+    "url": "/Yoavbenzvi/todo-app/static/js/2.c5ee11cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Yoavbenzvi/todo-app/static/js/2.c5ee11cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37e04be88b26e6e53ddf",
+    "url": "/Yoavbenzvi/todo-app/static/js/main.b4d6c727.chunk.js"
+  },
+  {
+    "revision": "4e4bfffe815684112a2e",
+    "url": "/Yoavbenzvi/todo-app/static/js/runtime-main.8ac04bb3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.94ee16e297c4258b1b15908aaba4cfe0.js"
+  "/Yoavbenzvi/todo-app/precache-manifest.3f4291988335c644d567b1c1451cf012.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Yoavbenzvi/todo-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
