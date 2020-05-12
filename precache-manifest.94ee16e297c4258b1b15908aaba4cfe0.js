0a99dc993e601c3bfd007df70478ab8d33ff37d7
Yoav <yoav.benzvi@gmail.com>
2020-05-12 16:24:03 +0200

    Updates

---
+++ precache-manifest.94ee16e297c4258b1b15908aaba4cfe0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e67305c951599255c752822ef66d094",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82a89618b46707eafbc8",
+    "url": "/static/css/main.60db9259.chunk.css"
+  },
+  {
+    "revision": "e471cd66f89e1d28ba69",
+    "url": "/static/js/2.c5ee11cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c5ee11cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82a89618b46707eafbc8",
+    "url": "/static/js/main.598b6a5f.chunk.js"
+  },
+  {
+    "revision": "310c168f5b4a8bb90b3d",
+    "url": "/static/js/runtime-main.eefa256f.js"
+  }
+]);
